4987598ac3316a1e2b7a259a5a97f218cfe4bed2
Joshi Devika (BD/TBC3) <jod1be@bosch.com>
2023-05-04 14:47:28 +0200

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-syntax-import-assertions",
+      [
+        "babel-plugin-rewrite-require",
+        {
+          aliases: {
+            crypto: "crypto-browserify",
+            stream: "stream-browserify",
+          },
+        },
+      ],
+    ],
   };
 };
